b63a8793f61e7019dd14e56ab73e780892502723
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-08-15 16:31:49 +0530

    HomePage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Navbar from './Navbar'
+import Home from './Home'
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
       <div className="Content">
-        <h1>App Component</h1>
+        <Home />
       </div>
     </div>
   );
