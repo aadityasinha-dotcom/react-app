5d694384c9402933de55ccbb258563edba690061
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-08-15 16:29:08 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './Navbar'
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <div className="Content">
         <h1>App Component</h1>
       </div>
