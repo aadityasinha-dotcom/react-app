506903a917a190a63473e0df3bc943e541e7af03
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-08-18 18:13:52 +0530

    click

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
 const Home = () => {
+
+    let name = 'Adi';
+
+    const handleClick = () => {
+        name = 'Aryan';
+    }
+
+    const handleClickAgain = (name) => {
+        console.log('hello' + name);
+    }
+
     return (
         <div className="home">
             <h2>HomePage</h2>
+            <p>{ name }</p>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={() => handleClickAgain('Adi')}>Click me again</button>
         </div>
     );
 }
