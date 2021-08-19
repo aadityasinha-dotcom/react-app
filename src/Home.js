bdc1c51fd3092c834d923c59aaa21f6f9a6c40dd
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-08-19 14:58:06 +0530

    changes

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,19 @@
-import { upstate } from 'react';
+import { upstate, useState } from 'react';
 
 const Home = () => {
     //let name = 'Adi';
     const [name, setName] = useState('Adi');
+    const [age, setAge] = useState(25);
 
     const handleClick = () => {
         setName('Aryan');
+        setAge(14);
     }
 
     return (
         <div className="home">
             <h2>HomePage</h2>
-            <p>{ name }</p>
+            <p>{ name } is { age } years old</p>
             <button onClick={handleClick}>Click me</button>
         </div>
     );
