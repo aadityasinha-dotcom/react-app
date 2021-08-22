740e5e20887f5fa30eea969735f40c43bfc630ad
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-08-22 19:22:49 +0530

    changes

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,5 +1,14 @@
 const BlogList = () => {
-    return (  );
+    return (
+        <div className="blog-list">
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                </div>
+            ))}
+        </div>
+    );
 }
  
 export default BlogList;
