f93e30fa6a8f7de2531d7a31dd3b5ef11b787acb
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-08-23 17:56:38 +0530

    props

--- src/Home.js
+++ src/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" />
-            <BlogList blogs={blogs} title="All Blogs!" />
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Adi Blogs!" />
         </div>
     );
 }
