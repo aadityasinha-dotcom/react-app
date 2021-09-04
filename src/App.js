27ce9ad307f7549d930888bbfee663dd0b7cf745
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-09-04 16:50:07 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import Home from './Home'
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
-      </div>
+    <div>
+      <label class="label" for="name">Enter name:</label>
+      <input id="name" type="text" />
+      <button style="background-color: blue; color: white;"></button>
     </div>
   );
 }
