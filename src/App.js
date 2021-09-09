250f6a39a3075959d8d811057bf6df2a50de2e8f
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-09-09 17:39:32 +0530

    style

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div>
       <label class="label" for="name">Enter name:</label>
       <input id="name" type="text" />
-      <button style={{backgroundColor: 'blue'}}>Submit</button>
+      <button style={{backgroundColor: 'blue', color:'white'}}>Submit</button>
     </div>
   );
 }
