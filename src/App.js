88b4e86468848b1e5a3916e559aa23dbe1f3e679
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-09-09 17:45:55 +0530

    java variables

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import Navbar from './Navbar'
 import Home from './Home'
 
 function App() {
+  const buttonText = 'Click Me!';
   return (
     <div>
       <label className="label" for="name">Enter name:</label>
       <input id="name" type="text" />
-      <button style={{backgroundColor: 'blue', color:'white'}}>Submit</button>
+      <button style={{backgroundColor: 'blue', color:'white'}}>{buttonText}</button>
     </div>
   );
 }
