7c7da5ff8881af325e07a17eb25c402ef2af6952
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-09-09 17:41:38 +0530

    classname

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './Home'
 function App() {
   return (
     <div>
-      <label class="label" for="name">Enter name:</label>
+      <label className="label" for="name">Enter name:</label>
       <input id="name" type="text" />
       <button style={{backgroundColor: 'blue', color:'white'}}>Submit</button>
     </div>
