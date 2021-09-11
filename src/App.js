918d6dcbfa19d8126e76214fd8caa91f6cab12b0
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-09-11 20:56:40 +0530

    text

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import Navbar from './Navbar'
 import Home from './Home'
 
-function getButtonText(){
-  return 'Click on me!';
-}
 
 function App() {
-  const buttonText = { text: "Click" };
+  const buttonText = {text: "Click"};
   return (
     <div>
       <label className="label" for="name">Enter name:</label>
       <input id="name" type="text" />
-      <button style={{backgroundColor: 'blue', color:'white'}}>
-        {buttonText}
-      </button>
+      <button style={{backgroundColor: 'blue', color:'white'}}>{buttonText.text}</button>
     </div>
   );
 }
