4bec99ebbe8590f94605d8537741d02b5b29b255
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-09-17 14:51:12 +0530

    changes

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -2,6 +2,21 @@ import React from 'react';
 
 const CommentDetail = () => {
     return (
-        
+        <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="Avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+          <img alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00PM</span>
+          </div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
     )
 }
