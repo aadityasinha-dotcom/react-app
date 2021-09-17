2e7852eca3a9c5e520364a7b1e876507960ce800
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-09-17 15:45:15 +0530

    changes

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -9,7 +9,7 @@ const CommentDetail = ( props ) => {
         </a>
         <div className="content">
           <a href="/" className="author">
-            Sam
+            {props.author}
           </a>
           <div className="metadata">
             <span className="date">Today at 6:00PM</span>
