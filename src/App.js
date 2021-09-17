7bf503616aa6d4e2174a74bcab8482d55ef86379
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-09-17 15:43:13 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import CommentDetail from './CommentDetail';
 function App() {
   return (
     <div className="ui container comments">
-      <CommentDetail />
+      <CommentDetail author='Sam' />
+      <CommentDetail author='Sam' />
+      <CommentDetail author='Sam' />
     </div>
   );
 }
