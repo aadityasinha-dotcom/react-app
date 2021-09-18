f9f93ea7b899530298ef809443da2d8fd951d356
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-09-18 10:27:54 +0530

    props

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const CommentDetail = () => {
+const CommentDetail = (props) => {
     return (
     <div className="comment">
         <a href="/" className="Avatar">
@@ -9,10 +9,10 @@ const CommentDetail = () => {
         </a>
         <div className="content">
           <a href="/" className="author">
-            Sam
+            {props.author}
           </a>
           <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
+            <span className="date">{props.timeAgo}</span>
           </div>
           <div className="text">Nice blog post!</div>
         </div>
