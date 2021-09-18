1178f70f7897352f54771f271c85d173eb7be821
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-09-18 10:30:25 +0530

    blogs

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import CommentDetail from './CommentDetail';
 function App() {
   return (
     <div className="ui container comments">
-      <CommentDetail author='Adi' timeAgo="Today at 4:45PM" />
-      <CommentDetail author='Aryan' timeAgo="Today at 2:00AM" />
-      <CommentDetail author='Raju' timeAgo="Yesterday at 5:00PM" />
+      <CommentDetail author='Adi' timeAgo="Today at 4:45PM" blog="Hello" />
+      <CommentDetail author='Aryan' timeAgo="Today at 2:00AM" blog="kya hai?" />
+      <CommentDetail author='Raju' timeAgo="Yesterday at 5:00PM" blog="kuch nahi :-(" />
     </div>
   );
 }
