9f4e67a691d06ec5ae2d2a98caff049f29cf9c65
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-09-18 10:44:31 +0530

    improved

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import faker from 'faker';
 
 const CommentDetail = (props) => {
     return (
     <div className="comment">
         <a href="/" className="Avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
           <img alt="avatar" />
         </a>
         <div className="content">
