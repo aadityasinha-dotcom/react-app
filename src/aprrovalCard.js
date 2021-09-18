bc79b40d9911ba273b2542a629460d1d5b13e3d6
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-09-18 13:06:06 +0530

    changes

--- src/aprrovalCard.js
+++ src/aprrovalCard.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const aprrovalCard = () => {
+const aprrovalCard = ( props ) => {
 
     return (
         <div className="ui card">
-            <div className="content">Are you sure?</div>
+            <div className="content">{props.children}</div>
             <div className="extra content">
                 <div className="ui two buttons">
                     <div className="ui basic green button">Approve</div>
