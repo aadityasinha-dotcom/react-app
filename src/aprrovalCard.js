3495a2c54b42ff8ee081b449de63245ade25ed08
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-09-18 11:36:52 +0530

    changes

--- src/aprrovalCard.js
+++ src/aprrovalCard.js
@@ -2,7 +2,17 @@ import react from 'react';
 
 const aprrovalCard = () => {
 
-    
+    return (
+        <div className="ui card">
+            <div className="content">Are you sure?</div>
+            <div className="extra content">
+                <div className="ui two buttons">
+                    <div className="ui basic green button">Approve</div>
+                    <div className="ui basic red button">Reject</div>
+                </div>
+            </div>
+        </div>
+    );
 
 };
 
