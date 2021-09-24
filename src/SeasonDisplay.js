411e27e7f0375553367359037a46cf0a7ee1522f
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-09-24 14:35:02 +0530

    changes

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const SeasonDisplay = () =>  {
-    window.navigator.geolocations.getCurrentPosition()
+    window.navigator.geolocations.getCurrentPosition(
+        
+    );
 
     return <div>Hi there!</div>
 }
