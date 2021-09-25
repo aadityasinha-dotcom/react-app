e177d280b68e91997f0238074de462beff7c412d
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-09-25 13:55:54 +0530

    changes

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,16 +1,11 @@
 import React from 'react';
 
-const SeasonDisplay = () =>  {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
-
-    return <div>Latitude: </div>;
-};
-
 class App extends React.Component {
     render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err)
+        );
         return <div>Latitude: </div>;
     }
 }
