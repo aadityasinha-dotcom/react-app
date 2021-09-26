d0ba0bbbae7e971658e8ef1baed41db1be4fdab6
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-09-26 18:11:39 +0530

    changes

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 class App extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    
+    
     render() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => console.log(position),
