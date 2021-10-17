86a7aca1dcd021a60bb32363211f9c43b10b08a4
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-10-17 17:46:58 +0530

    seasonConfig1

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -21,7 +21,7 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = props => {
   const season = getSeason(props.lat, new Date().getMonth());
-  seasonConfig
+  const { text, iconName} = seasonConfig[season];  // { text, iconName }
 
   return (
     <div>
