d4e7336012c251b50b64a6ae3b85aa8af3f745be
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-10-17 17:55:17 +0530

    styling

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -25,7 +25,7 @@ const SeasonDisplay = props => {
   const { text, iconName} = seasonConfig[season];  // { text, iconName }
 
   return (
-    <div>
+    <div className={'season-display ${season}'}>
       <i className={`icon-left massive${iconName} icon`} />
       <h1>{text}</h1>
       <i className={`icon-right massive${iconName} icon`} />
