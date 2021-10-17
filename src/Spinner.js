6ab73ccdcfdea7900d2583d059dfaa9343d358c0
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-10-17 18:37:56 +0530

    spinner message

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Spinner = () => {
+const Spinner = (props) => {
     return(
         <div class="ui active dimmer">
             <div class="ui big text loader">
-                Loading...
+                {props.message}
             </div>
         </div>
     );
