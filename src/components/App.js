e337c43689ecec387d25c18ef9fa5f4f47f10220
aadityasinha-dotcom <aadityasinha009@gmail.com>
2022-01-20 21:45:44 +0530

    routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-
-const PageOne = () => {
-  return (
-    <div>
-      PageOne
-      <button>
-          <a href="/pagetwo">Navigate to Page Two</a>
-      </button>
-    </div>
-  );
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-      PageTwo
-      <button>
-        <a href="/">Navigate to Page One</a>
-      </button>
-    </div>
-  );
-};
+import artwork from '../contents/artwork';
+import bio from '../contents/bio';
+import contacts from '../contents/contacts';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" component={PageTwo} />
+          
         </div>
       </BrowserRouter>
     </div>
