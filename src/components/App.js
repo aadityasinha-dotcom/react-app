9014e0699105e2939a2a89696849f5ad4fda30ee
aadityasinha-dotcom <aadityasinha009@gmail.com>
2022-01-20 21:33:42 +0530

    router

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,38 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 const PageOne = () => {
-    return <div>PageOne</div>;
+  return (
+    <div>
+      PageOne
+      <button>
+          <a href="/pagetwo">Navigate to Page Two</a>
+      </button>
+    </div>
+  );
 };
 
 const PageTwo = () => {
-    return <div>PageTwo</div>;
+  return (
+    <div>
+      PageTwo
+      <button>
+        <a href="/">Navigate to Page One</a>
+      </button>
+    </div>
+  );
 };
 
 const App = () => {
-    return (
+  return (
     <div>
-        <BrowserRouter>
-            <div>
-            <Route path="/" exact component={PageOne} />
-            <Route path="/PageTwo" component={PageTwo} />
-            </div>
-        </BrowserRouter>
-    </div>);
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
