18537aaaa3ef387a8df8bd7dc43305dba1fef6a9
aadityasinha-dotcom <aadityasinha009@gmail.com>
2022-01-20 22:01:28 +0530

    homepage

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,14 @@ import artwork from '../contents/artwork';
 import bio from '../contents/bio';
 import contacts from '../contents/contacts';
 import main from '../contents/main';
+import Header from './Header';
 
 const App = () => {
   return (
     <div className="ui container">
-      <Header />
       <BrowserRouter>
         <div>
+          <Header />
           <Route path="/" exact component={main} />
           <Route path="/main/bio" exact component={bio} />
           <Route path="/main/artwork" exact component={artwork} />
