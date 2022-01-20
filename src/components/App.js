4ba6452cf184b487fc8b2e5d842ab90d63752ac2
aadityasinha-dotcom <aadityasinha009@gmail.com>
2022-01-20 21:19:06 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { BrowserRouter}
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const PageOne = () => {
+    return <div>PageOne</div>;
+};
+
+const PageTwo = () => {
+    return <div>PageTwo</div>;
+};
 
 const App = () => {
     return <div>App</div>;
