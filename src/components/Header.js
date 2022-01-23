7076506939baa789454a3b3e09ffe0193cdb6aad
aadityasinha-dotcom <aadityasinha009@gmail.com>
2022-01-23 20:09:33 +0530

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,20 @@ const Header = () => {
     return (
         <nav className="navbar">
             <ul className="navbar-nav"></ul>
+            <div class="ui vertical menu">
+                <div class="item">
+                    <Link to="/" class="header">Products</Link>
+                </div>
+                <div class="item">
+                    <Link to="/artwork" class="header">CMS Solutions</Link>
+                </div>
+                <div class="item">
+                    <Link to="/bio" class="header">Hosting</Link>
+                </div>
+                <div class="item">
+                    <Link to="/contacts" class="header">Support</Link>
+                </div>
+            </div>
         </nav>
     );
 };
