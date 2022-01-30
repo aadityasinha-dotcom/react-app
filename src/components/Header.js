c58fb7b62de17530fdffb40ae33b5174b1525394
aadityasinha-dotcom <aadityasinha009@gmail.com>
2022-01-30 18:14:57 +0530

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import './App.css';
+import Navbar from '../Navbar/index';
+import { useState } from 'react';
 
 const Header = () => {
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    } 
+
     return (
-        <nav className="navbar">
-            <ul className="navbar-nav"></ul>
-            <div class="ui vertical menu">
-                <div class="item">
-                    <Link to="/" class="header">HomePage</Link>
-                </div>
-                <div class="item">
-                    <Link to="/artwork" class="header">Artwork</Link>
-                </div>
-                <div class="item">
-                    <Link to="/bio" class="header">Bio</Link>
-                </div>
-                <div class="item">
-                    <Link to="/contacts" class="header">Contacts</Link>
-                </div>
-            </div>
-        </nav>
+        <>
+            <Navbar toggle={toggle} />
+        </>
     );
 };
 
